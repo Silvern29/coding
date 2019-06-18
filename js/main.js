a23829b19221969ad9a38f2968293cfb0fecdaee
Florian Redlinghaus <florian@redlinghaus.at>
2019-06-19 00:00:55 +0200

    looks great

--- js/main.js
+++ js/main.js
@@ -2,13 +2,22 @@ function splitScroll(){
   const controller = new ScrollMagic.Controller();
 
   new ScrollMagic.Scene({
-    duration: '200%',
-    triggerElement: '.title',
-    triggerHook: 0
+    duration: '300%',
+    triggerElement: '#courses-pin',
+    triggerHook: 0,
   })
-    .setPin('.title')
+    .setPin('#courses-pin')
   .addIndicators()
   .addTo(controller);
+
+  new ScrollMagic.Scene({
+    duration: '200%',
+    triggerElement: '#projects-pin',
+    triggerHook: 0,
+  })
+    .setPin('#projects-pin')
+    .addIndicators()
+    .addTo(controller);
 }
 
 splitScroll();
