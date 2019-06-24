b75b044b4b5c36e039b1821e3ce49efeef503f03
Florian Redlinghaus <florian@redlinghaus.at>
2019-06-24 23:01:04 +0200

    reload bug fixed

--- js/main.js
+++ js/main.js
@@ -20,13 +20,33 @@ function splitScroll(){
     .addTo(controller);
 }
 
-
-
-  window.onresize = function() {
-    var myUrl = window.location;
-    window.location.replace(myUrl)
-  }
-
 if (document.documentElement.clientWidth > 822) {
   splitScroll();
+}
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+var cStatus;
+
+if (document.documentElement.clientWidth >= 822) {
+  cStatus = 'large';
+} else if (document.documentElement.clientWidth < 822) {
+  cStatus = 'small';
+}
+
+let cReload = () => {
+  var myUrl = window.location;
+  window.location.replace(myUrl);
+}
+
+window.onresize = function() {
+  if ((cStatus != 'small' && document.documentElement.clientWidth < 822) || (cStatus != 'large' && document.documentElement.clientWidth >= 822)) {
+    cReload();
+  }
 }
